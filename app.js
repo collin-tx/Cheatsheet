fb4ee0244f472d12009fb56bd112e55ce898d6ad
Collin Miller Smith <cmsmith.tx@gmail.com>
2019-07-02 17:03:26 -0500

    Added js for grid buttons to function

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const showButton = document.querySelector(".show-btn");
 const hideButton = document.querySelector(".hide-btn");
 const table = document.querySelector('.div-1');
 
+const grid = document.querySelector('.css-grid');
+const gridShowButton = document.querySelector(".show-grid-btn");
+const gridHideButton = document.querySelector(".hide-grid-btn");
+
 // displays table
 function showTable(e) {
     table.classList.remove('hide');
@@ -16,3 +20,20 @@ function hideTable(e) {
 
 //when clicked, table will disappear
 hideButton.addEventListener('click', hideTable);
+
+
+
+// displays grid
+function showGrid(e) {
+    grid.classList.remove('hide');
+}
+// when clicked, grid will appear
+gridShowButton.addEventListener("click", showGrid);
+
+// Hides grid
+function hideGrid(e) {
+    grid.classList.add('hide');
+}
+
+//when clicked, table will disappear
+gridHideButton.addEventListener('click', hideGrid);
